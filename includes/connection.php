65572bd26e76e60673e1836879ee6197d14130b1
caitlinfrancis <58597678+caitlinfrancis@users.noreply.github.com>
2022-04-28 08:51:30 -0500

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,5 @@
 <?php
 
-$connection = mysqli_init(); mysqli_ssl_set($connection,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($connection, "mikethefloorguy.mysql.database.azure.com", "mikethefloorguy", "Poop!@34", "floorguy_database", 3306, MYSQLI_CLIENT_SSL);
+$connection = mysqli_init(); mysqli_ssl_set($connection,NULL,NULL, "c:\ssl\BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); mysqli_real_connect($connection, "mikethefloorguy.mysql.database.azure.com", "mikethefloorguy", "Poop!@34", "floorguy_database", 3306, MYSQLI_CLIENT_SSL);
+
 ?>
