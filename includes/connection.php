d3c314f0fa577847987eb7a46375492f59a9f42a
caitlinfrancis <58597678+caitlinfrancis@users.noreply.github.com>
2022-06-12 16:13:11 -0500

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,18 +1,5 @@
 <?php
 
-/*$server     = "localhost";
-$username   = "mikethef_loorguy";
-$password   = "TheShit!@3642";
-$db         = "mikethef_loorguy";
-
-
-$connection = mysqli_connect( $server, $username, $password, $db );
-
-// check connection
-if( !$connection ) {
-    die( "Connection failed: " . mysqli_connect_error() );
-}*/
-
 $server     = "localhost";
 $username   = "root";
 $password   = "";
